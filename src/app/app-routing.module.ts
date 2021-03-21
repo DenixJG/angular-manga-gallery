7d4e3822020a2396fd0f554ca6f78c4f8145004a
Rafael Popescu <rafael.popescu140@gmail.com>
2021-03-21 14:48:30 +0100

    Agregado servicios y completando el formulario para mangas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtistFormComponent } from './components/artist-form/artist-form.component';
+import { AuthorFormComponent } from './components/author-form/author-form.component';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MangaDetailsComponent } from './components/manga-details/manga-details.component';
+import { MangaFormComponent } from './components/manga-form/manga-form.component';
+import { MangaLibraryComponent } from './components/manga-library/manga-library.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'manga-library', component: MangaLibraryComponent },
+  { path: 'manga-library/manga/:id', component: MangaDetailsComponent },
+  { path: 'manga-library/new', component: MangaFormComponent },
+  { path: 'author/new', component: AuthorFormComponent },
+  { path: 'artist/new', component: ArtistFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MangaDetailsComponent } from './components/manga-details/manga-details.component';
+import { MangaLibraryComponent } from './components/manga-library/manga-library.component';
+import { MangaFormComponent } from './components/manga-form/manga-form.component';
+import { AuthorFormComponent } from './components/author-form/author-form.component';
+import { ArtistFormComponent } from './components/artist-form/artist-form.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { MaterialModules } from './material.module'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MangaDetailsComponent,
+    MangaLibraryComponent,
+    MangaFormComponent,
+    AuthorFormComponent,
+    ArtistFormComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
